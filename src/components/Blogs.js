963d886ba3a3a4b1b5393c4e328d2dfe8e070f4e
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-09-19 15:38:24 +0300

    MUI table: blogs (NOTE: link should be in row/cell, cusomize width is cons)

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { initializeObjs } from '../reducers/blogsReducer'
 import { Link } from 'react-router-dom'
 
+import { TableContainer, Paper, Table, TableBody } from '@mui/material'
+import { TableRow, TableCell } from '@mui/material'
+
 const Blogs = () => {
   const blogs = useSelector(({ blogs }) => {
     const _blogs = [...blogs]
@@ -16,29 +19,43 @@ const Blogs = () => {
 
   return (
     <>
-      {blogs.map((o) => (
+      {/* {blogs.map((o) => (
         <Link key={o.id} to={`/blogs/${o.id}`}>
           <Blog className="blog" key={o.id} blog={o} />
         </Link>
-      ))}
+      ))} */}
+
+      {/* NOTE: Link should be in row */}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableBody>
+            {blogs.map((o) => (
+              <Blog key={o.id} className="blog" blog={o} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
 export default Blogs
 
 const Blog = (props) => {
-  const blogStyle = {
-    paddingTop: 5,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5,
-  }
   return (
-    <div style={blogStyle} className="blog">
-      <div>
-        {props.blog.title} - {props.blog.author}
-      </div>
-    </div>
+  // <div style={blogStyle} className="blog">
+  //   <div>
+  //     {props.blog.title} - {props.blog.author}
+  //   </div>
+  // </div>
+
+  // NOTE: Link should be in row
+    <TableRow>
+      <TableCell>
+        <Link to={`/blogs/${props.blog.id}`}>{props.blog.title}</Link>
+      </TableCell>
+      <TableCell>
+        {props.blog.author}
+      </TableCell>
+    </TableRow>
   )
 }
