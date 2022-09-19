5ddb51d0d3896a68f5b852f4928fa460c929880c
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-09-19 10:06:21 +0300

    custom hook / util input field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { loginUser } from '../reducers/userReducer'
+import { useField } from './utils/hooks'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+  const username = useField('text')
+  const password = useField('password')
 
   const dispatch = useDispatch()
 
   const handleLogin = async (event) => {
     event.preventDefault()
-    dispatch(loginUser(username, password))
+    // dispatch(loginUser(username, password))
+    dispatch(loginUser(username.fields.value, password.fields.value))
   }
 
   return (
     <form onSubmit={handleLogin}>
-      <div>
+      {/* <div>
         username
         <input type="text" id="username" placeholder="username" value={username}
           onChange={({ target }) => setUsername(target.value)}
@@ -26,7 +31,17 @@ const LoginForm = () => {
         <input type="password" id="password" placeholder="password" value={password}
           onChange={({ target }) => setPassword(target.value)}
         />
+      </div> */}
+
+      <div>
+        username
+        <input {...username.fields} />
       </div>
+      <div>
+        password
+        <input {...password.fields} />
+      </div>
+
       <button type="submit">
         login
       </button>
